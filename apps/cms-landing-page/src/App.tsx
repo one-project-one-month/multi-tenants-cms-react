04d145a484a57208bc98b41f33a0d30536343dd4
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-07-04 02:23:19 +0630

    initial turbo set up

---
+++ apps/cms-landing-page/src/App.tsx
@@ -0,0 +1,15 @@
+import "@cms/ui/styles/globals.css";
+
+function LandingPageContent() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-2">This is a cms landing page</h1>
+    </div>
+  );
+}
+
+function App() {
+  return <LandingPageContent />;
+}
+
+export default App;
