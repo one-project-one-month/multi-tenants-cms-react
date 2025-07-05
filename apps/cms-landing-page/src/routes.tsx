dca301889f3583eedb9535a076b5996a15fbf895
Kyaw Zin Thant <kyawthantver2@gmail.com>
2025-07-05 15:36:43 +0630

    :sparkles: feat(CMS-1-40: Implement login/register UI):

--- apps/cms-landing-page/src/routes.tsx
+++ apps/cms-landing-page/src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, redirect } from 'react-router';
 import RootLayout from './components/RootLayout';
 import Loading from './components/Loading';
 import { HomeLoader } from './router/loader/data-loader';
+import AuthLayout from './features/auth/components/AuthLayout';
 
 const LandingPage = lazy(() => import('./page/index'));
 const LoginPage = lazy(() => import('./features/auth/login'));
@@ -34,14 +35,21 @@ export const router = createBrowserRouter([
   },
 
   {
-    path: '/login',
-    element: <LoginPage />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        path: 'login',
+        element: withSuspense(LoginPage),
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+    ],
   },
-  {
-    path: '/register',
-    element: <LoginPage />,
-    loader: RegisterPage,
-  },
+
   {
     path: '/logout',
 
