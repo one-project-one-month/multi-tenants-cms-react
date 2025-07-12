b59f9f858fc1d14ab7f87d9c6c5b0f8d9392b7cb
PHYOPHYO2397 <phyophyomyatthu3@gmail.com>
2025-07-13 01:26:20 +0900

    :sparkles: feat(lms client setup): lMS Client Setup
    LMS Client Setup

--- apps/lms-client/src/main.tsx
+++ apps/lms-client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryProviders } from './lib/QueryProvider.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes.tsx';
+import '@cms/ui/styles/globals.css';
+//import { createApiClient, setApiClient } from '@cms/data';
+//setApiClient(createApiClient(import.meta.env.VITE_BACKEND_SERVER));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryProviders>
+      <RouterProvider router={router} />
+    </QueryProviders>
+  </StrictMode>
+);
