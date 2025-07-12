3b75591555e7412822473a948b8d9d227455b0be
thanthtooaung <thanthtoo1285@gmail.com>
2025-07-12 14:20:42 +0700

    :sparkles: feat(setup lms admin layout): setup lms admin layout
    setup lms admin layout including sidebar, navbar and necessary routes

---
+++ apps/lms-dashboard/src/components/Layout/data/sidebar-data.ts
@@ -0,0 +1,67 @@
+import {
+  IconFileAnalytics,
+  IconHelp,
+  IconLayoutDashboard,
+  IconSettings,
+  IconTool,
+  IconUserCog,
+  IconBook,
+  IconTags
+} from '@tabler/icons-react';
+import type { SidebarData } from '../types';
+
+export const sidebarData: SidebarData = {
+  navGroups: [
+    {
+      title: 'General',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/',
+          icon: IconLayoutDashboard,
+        },
+        {
+          title: 'Report',
+          url: '/report',
+          icon: IconFileAnalytics,
+        },
+        {
+          title: 'Course',
+          url: '/course',
+          icon: IconBook,
+        },
+        {
+          title: 'Category',
+          url: '/category',
+          icon: IconTags,
+        },
+      ],
+    },
+    {
+      title: 'Other',
+      items: [
+        {
+          title: 'Settings',
+          icon: IconSettings,
+          items: [
+            {
+              title: 'Profile',
+              url: '/settings',
+              icon: IconUserCog,
+            },
+            {
+              title: 'Account',
+              url: '/settings/account',
+              icon: IconTool,
+            },
+          ],
+        },
+        {
+          title: 'Help Center',
+          url: '/help-center',
+          icon: IconHelp,
+        },
+      ],
+    },
+  ],
+};
