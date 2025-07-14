377a8bcd7794512ae6739580874ff1efda8d2d45
mu phaw <muphaw755@gmail.com>
2025-07-14 18:01:09 +0630

    :bug: fix(fix header(nav)): fix header(nav)

--- apps/cms-landing-page/src/page/Error.tsx
+++ apps/cms-landing-page/src/page/Error.tsx
@@ -20,7 +20,6 @@ function ErrorPage() {
 
   return (
     <div className="flex min-h-screen flex-col overflow-hidden">
-      <Header />
       <main className="mx-auto w-[400px] my-32 flex flex-1 items-center justify-center">
         <Card className="w-full max-w-md text-center shadow-xl">
           <CardHeader className="flex flex-col items-center gap-4">
